18018650be0a94d16af19b0770a2712d29daa9fd
John F Morton <morton@jmx2.com>
2023-07-26 14:19:07 -0400

    Removed altinity/clickhouse-backup in favor of native backup script

--- backup-data.sh
+++ backup-data.sh
@@ -10,26 +10,44 @@
 # Load the environment variables
 source .env
 
-# docker run -u $(id -u clickhouse) --rm -it --network host -v "/var/lib/clickhouse:/var/lib/clickhouse" \
-#    -e CLICKHOUSE_PASSWORD="password" \
-#    -e S3_BUCKET="clickhouse-backup" \
-#    -e S3_ACCESS_KEY="access_key" \
-#    -e S3_SECRET_KEY="secret" \
-#    altinity/clickhouse-backup --help
-
 # Create the local backup directory if is does not exist
 mkdir -p ${LOCAL_BACKUP_PATH}${LOCAL_CLICKHOUSE_PATH}
 
 # create a timestamp variable
 timestamp=$(date +%Y-%m-%d-%H-%M-%S)
 
-# The command must run as root to access the internal clickhouse data backup directory
-docker run --rm -it --network analytics_default -v "${LOCAL_BACKUP_PATH}${LOCAL_CLICKHOUSE_PATH}:/var/lib/clickhouse/backup/" \
-   -e CLICKHOUSE_HOST="plausible_events_db" \
-   altinity/clickhouse-backup create clickhouse-backup-${timestamp}
+
+# Define the container name
+# Look for POSTGRES_CONTAINER_NAME and set to default if not found
+if [ -z ${CLICKHOUSE_CONTAINER_NAME+x} ]; then
+    CLICKHOUSE_CONTAINER_NAME="analytics-plausible_events_db-1"
+fi
+
+# Look for env LOCAL_BACKUP_RETENTION_DAYS and set default if not found
+if [ -z ${LOCAL_BACKUP_RETENTION_DAYS+x} ]; then
+    LOCAL_BACKUP_RETENTION_DAYS=7
+fi
+
+
+# clickhouse-client --query "BACKUP DATABASE plausible_events_db TO Disk('backups', 'plausible_events_db_backup.zip')"
+docker exec -it $CONTAINER_NAME clickhouse-client --query "BACKUP DATABASE plausible_events_db TO Disk('backups', 'plausible_events_db_backup_${timestamp}.zip')"
+
+# Copy the backup file from Docker container to the server
+docker cp $CONTAINER_NAME:/var/lib/clickhouse/backups/plausible_events_db_backup_${timestamp}.zip ${LOCAL_BACKUP_PATH}${LOCAL_CLICKHOUSE_PATH}
+
+# Remove the backup file from inside the Docker container
+docker exec $CONTAINER_NAME /bin/bash -c "rm /var/lib/clickhouse/backups/plausible_events_db_backup_${timestamp}.zip"
 
 # change the ownership of the created backup files to the forge user
-chown -R forge:forge /home/forge/analytics/backups/clickhouse-data
+chown -R forge:forge ${LOCAL_BACKUP_PATH}${LOCAL_POSTGRES_PATH}
 
-# delete backups older than 30 days
-find ${LOCAL_BACKUP_PATH}${LOCAL_CLICKHOUSE_PATH} -type f -mtime +30 -name '*.tar' -delete
+echo 'Pruning old Plausible Event database backups to max age of '${LOCAL_BACKUP_RETENTION_DAYS}' days.'
+find ${LOCAL_BACKUP_PATH}${LOCAL_POSTGRES_PATH} -type f -mtime +${LOCAL_BACKUP_RETENTION_DAYS} -exec rm {} \;
+
+# check for success
+if [ $? -eq 0 ]; then
+    echo "Successfully backed up the Plausible Event database to ${LOCAL_BACKUP_PATH}${LOCAL_POSTGRES_PATH}."
+else
+    echo "Failed to backup the Plausible Event database."
+    exit 1
+fi
